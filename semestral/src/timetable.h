99b9449559bc77729114b53ec237d61893b28e1a
Michal Dobeš <dobesmic@fit.cvut.cz>
2023-03-29 11:33:57 +0200

    semestral: Implement data structures representing timetable entries

--- semestral/src/timetable.h
+++ semestral/src/timetable.h
@@ -3,17 +3,26 @@
 
 #include "timeinterval.h"
 
+#include <cstdint>
 #include <vector>
+#include <map>
+#include <string>
 
 struct Entry {
-
+    uint32_t id;
     std::vector<TimeInterval> timeslots;
-
 };
 
+template <typename T>
+using dictionary = std::map<std::string, T>;
+
 class Subject {
+    std::string code;
+    dictionary<std::vector<Entry>> entries;
+};
 
-
-}
+class Timetable {
+    dictionary<dictionary<uint16_t>> schedule;
+};
 
 #endif /* TIMETABLE_H */
